2c281802d18a0439bfeaaae6af3d6328026c2f2e
mixiaobaozi <2206124398@qq.com>
2024-09-22 12:06:34 +0800

    轮子初修

--- kubejs/server_scripts/ProjectE/emc.js
+++
@@ -1,3 +0,0 @@
-ServerEvents.recipes(e=>{
-    e.remove({ mod: 'projecte' })
-})
---
+++ kubejs/server_scripts/util/formula.js
@@ -0,0 +1,51 @@
+// priority: 100
+///自然灵气神灵仪式
+/**
+ * 
+ * @param {string} input 
+ * @param {string} start_item 
+ * @param {string} output 
+ */
+function offering(input,start_item,output){
+    event.custom({
+        "type": "naturesaura:offering",
+        "input": {
+        "item": input
+        },
+        "start_item": {
+        "item": start_item
+        },
+        "output": {
+        "item": output
+        }
+    })
+},
+///自然灵气祭坛
+/**
+ * 
+ * @param {object} input 
+ * @param {object} output 
+ * @param {string} catalyst 
+ * @param {number} aura 
+ * @param {number} time 
+ */
+function altar(input,output,catalyst,aura,time){
+    event.custom({
+        "type": "naturesaura:altar",
+        "input": {
+        "item": input[0],
+        "count": input[1]
+        },
+        "output": {
+        "item": output[0],
+        "count": output[1]
+        },
+        "catalyst": {
+        "item": catalyst
+        },
+        "aura": aura,
+        "time": time    
+    })
+},
+
+
